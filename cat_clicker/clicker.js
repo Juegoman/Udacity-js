dc3fb5140aaad960bc1a87b270ff1b3f41d07e5c
Ruben Gallegos <gallegos_ruben@yahoo.com>
2016-08-24 01:59:44 -0700

    Changed javascript code to the MVC pattern. Added basic persistance. Added update and add features.
    Javascript code has been refactored to conform to the
    Model/View/Controller design pattern. Persistance has been implemented by
    storing pepes into localStorage. Update and Create features have been
    added through the addition of a form to the HTML and corresponding
    javascript.

--- cat_clicker/clicker.js
+++ cat_clicker/clicker.js
@@ -1,73 +1,181 @@
-var pepeID, $count, ind, insertHTML, insertPepe;
-//list of pepes
-var pepeList = [
-  {
-    name: "ASCII Pepe",
-    image: "ascii-pepe.gif",
-    clicks: 0
-  },
-  {
-    name: "Original Pepe",
-    image: "orig-pepe.jpg",
-    clicks: 0
-  },
-  {
-    name: "Extremely Rare Limited Edition Misprint Pepe",
-    image: "misprintpepe.gif",
-    clicks: 0
-  },
-  {
-    name: "Dark Side of the Pepe",
-    image: "illumpepe.jpg",
-    clicks: 0
-  },
-  {
-    name: "Pepe the Painter",
-    image: "painterpepe.jpg",
-    clicks: 0
-  }
-];
-
 $(document).ready(function() {
-  //populate the right list of pepes
-  for (var i = 0; i < pepeList.length; i++) {
-    pepeID = 'pepelink' + i;
-    insertHTML = '' +
-    '<a href="#" id="' + pepeID + '">' +
-      pepeList[i].name +
-      '<img alt="' + pepeList[i].name + '" src="' + pepeList[i].image + '">' +
-    '</a>';
-    $(".pepe-list").append(insertHTML);
-  }
-
-  //click handler for bringing up a pepe on the left side of the screen.
-  $(".pepe-list a").click(function(e) {
-    //unbind events to prevent memleaks and clear the right side.
-    $(".pepe-container img").off();
-    $(".pepe-container").html('');
-    //get the index of the pepe selected.
-    ind = parseInt($(this).attr("id").replace("pepelink", ''));
-    //build the HTML to insert.
-    insertPepe = '' +
-    '<div class="pepe">' +
-      '<h2 class="name">' + pepeList[ind].name + '</h2>' +
-      '<h3>' +
-        'This pepe has been memed <span class="count">' + pepeList[ind].clicks + '</span> times.' +
-      '</h3>' +
-      '<img alt="' + pepeList[ind].name + '" src="' + pepeList[ind].image + '">' +
-    '</div>';
-    //append the HTML and attach the click listener.
-    $(".pepe-container").append(insertPepe);
-    $(".pepe-container img").on("click", pepeClickEvent(ind));
-  });
-});
-
-//closure for click event.
-var pepeClickEvent = function(index) {
-  return function() {
-    //get the counter, increment the count and update the counter.
-    $count = $(this).parent().find(".count");
-    pepeList[index].clicks += 1;
-    $count.text(pepeList[index].clicks + '');
+  var model = {
+    init: function() {
+      if (!localStorage.pepes) {
+        localStorage.pepes = JSON.stringify(this.pepeList);
+      }
+      this.currentPepe = 0;
+    },
+    incrementCurrentPepe: function() {
+      var pepes = JSON.parse(localStorage.pepes);
+      pepes[this.currentPepe].clicks += 1;
+      localStorage.pepes = JSON.stringify(pepes);
+    },
+    getPepeList: function() {
+      return JSON.parse(localStorage.pepes);
+    },
+    getCurrentPepe: function() {
+      return JSON.parse(localStorage.pepes)[this.currentPepe];
+    },
+    updateCurrentPepe: function(index) {
+      this.currentPepe = index;
+    },
+    updatePepe: function(name, imgSrc) {
+      var pepes = this.getPepeList();
+      pepes[this.currentPepe].name = name;
+      pepes[this.currentPepe].image = imgSrc;
+      localStorage.pepes = JSON.stringify(pepes);
+    },
+    addPepe: function(newName, newImgSrc) {
+      var pepes = this.getPepeList();
+      var newPepe = {
+        name: newName,
+        image: newImgSrc,
+        clicks: 0
+      };
+      pepes.push(newPepe);
+      localStorage.pepes = JSON.stringify(pepes);
+    },
+    pepeList: [
+      {
+        name: "ASCII Pepe",
+        image: "ascii-pepe.gif",
+        clicks: 0
+      },
+      {
+        name: "Original Pepe",
+        image: "orig-pepe.jpg",
+        clicks: 0
+      },
+      {
+        name: "Extremely Rare Limited Edition Misprint Pepe",
+        image: "misprintpepe.gif",
+        clicks: 0
+      },
+      {
+        name: "Dark Side of the Pepe",
+        image: "illumpepe.jpg",
+        clicks: 0
+      },
+      {
+        name: "Pepe the Painter",
+        image: "painterpepe.jpg",
+        clicks: 0
+      }
+    ]
   };
-};
+
+  var view = {
+    init: function() {
+      this.$pepeList = $(".pepe-list");
+      this.$pepeContainer = $(".pepe-container");
+      this.initForm();
+      this.populatePepeList();
+    },
+    initForm: function() {
+      $("#open").on("click", function() {view.showAddForm();});
+      $("#cancel").on("click", function() {view.hideAddForm();});
+      $("#update").on("click", function() {view.updatePepeEvent();});
+      $("#add").on("click", function() {view.addPepeEvent();});
+    },
+    populatePepeList: function() {
+      $(".pepe-list a").off();
+      this.$pepeList.html('');
+      var pepeList = controller.getPepeList();
+      for (var i = 0; i < pepeList.length; i++) {
+        var insertHTML = '' +
+        '<a href="#">' +
+          pepeList[i].name +
+          '<img alt="' + pepeList[i].name + '" src="' + pepeList[i].image + '">' +
+        '</a>';
+        this.$pepeList.append(insertHTML);
+        $($(".pepe-list a")[i]).on("click", controller.pepeListClickEvent(i));
+      }
+    },
+    renderPepe: function() {
+      var pepe = controller.getCurrentPepe();
+      this.$pepeContainer.html('');
+      var insertHTML = '' +
+      '<div class="pepe">' +
+        '<h2 class="name">' + pepe.name + '</h2>' +
+        '<h3>' +
+          'This pepe has been memed <span class="count">' + pepe.clicks + '</span> times.' +
+        '</h3>' +
+        '<img alt="' + pepe.name + '" src="' + pepe.image + '">' +
+      '</div>';
+      this.$pepeContainer.append(insertHTML);
+      this.updateForm();
+    },
+    updateCount: function() {
+      $(".count").text(controller.getCurrentPepe().clicks + '');
+    },
+    updateForm: function() {
+      pepe = controller.getCurrentPepe();
+      $("#name").val(pepe.name);
+      $("#imgsrc").val(pepe.image);
+    },
+    showAddForm: function() {
+      this.updateForm();
+      $("form").slideDown();
+    },
+    hideAddForm: function() {
+      $("form").slideUp();
+      $("#name").val('');
+      $("#imgsrc").val('');
+    },
+    updatePepeEvent: function() {
+      var newName = $("#name").val();
+      var newImgSrc = $("#imgsrc").val();
+      controller.updatePepe(newName, newImgSrc);
+      this.hideAddForm();
+      this.populatePepeList();
+      this.renderPepe();
+    },
+    addPepeEvent: function() {
+      var name = $("#name").val();
+      var imgSrc = $("#imgsrc").val();
+      controller.addPepe(name, imgSrc);
+      this.hideAddForm();
+      this.populatePepeList();
+      this.renderPepe();
+    }
+  };
+
+  var controller = {
+    init: function() {
+      model.init();
+      view.init();
+    },
+    getPepeList: function() {
+      return model.getPepeList();
+    },
+    getCurrentPepe: function() {
+      return model.getCurrentPepe();
+    },
+    pepeListClickEvent: function(index) {
+      return function() {
+        $(".pepe-container img").off();
+        controller.setCurrentPepe(index);
+        view.renderPepe();
+        $(".pepe-container img").on("click", controller.pepeClickEvent());
+      };
+    },
+    pepeClickEvent: function() {
+      return function() {
+        model.incrementCurrentPepe();
+        view.updateCount();
+      };
+    },
+    setCurrentPepe: function(index) {
+      model.updateCurrentPepe(index);
+    },
+    updatePepe: function(name, imgSrc) {
+      model.updatePepe(name, imgSrc);
+    },
+    addPepe: function(name, imgSrc) {
+      model.addPepe(name, imgSrc);
+    }
+  };
+
+  controller.init();
+});
