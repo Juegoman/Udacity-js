15933d342eda920c839e7c39b4aaded9fcb6f24c
Ruben Gallegos <gallegos_ruben@yahoo.com>
2016-08-25 20:09:19 -0700

    Added priority feature.
    Added priority feature to the app, where each todo item can be
    prioritized. The prioritized state is stored in the model, and a toggle
    function for the priority was added as well. A prioritize button was added
    in the html template. In the todo-view file the priority feature was
    hooked up with functions to toggle the priority, their event bindings and
    changes to the render function to facilitate the feature.

--- todo/js/models/todo.js
+++ todo/js/models/todo.js
@@ -8,18 +8,27 @@ var app = app || {};
 	// ----------
 
 	// Our basic **Todo** model has `title`, `order`, and `completed` attributes.
+	// Model also has the prioritized attribute.
 	app.Todo = Backbone.Model.extend({
 		// Default attributes for the todo
-		// and ensure that each todo created has `title` and `completed` keys.
+		// and ensure that each todo created has `title`, `completed`, and `completed` keys.
 		defaults: {
 			title: '',
-			completed: false
+			completed: false,
+			prioritized: false
 		},
 
 		// Toggle the `completed` state of this todo item.
 		toggle: function () {
 			this.save({
 				completed: !this.get('completed')
+			});
+		},
+
+		// Toggle the `prioritized` state of this todo item.
+		togglePrioritized: function () {
+			this.save({
+				prioritized: !this.get('prioritized')
 			});
 		}
 	});
